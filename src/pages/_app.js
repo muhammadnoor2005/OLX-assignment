8c06004e2fbc2ad9c7f25324e669a89c844158b9
muhammadnoor2005 <125201339+muhammadnoor2005@users.noreply.github.com>
2023-10-14 21:05:06 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,8 @@
 
-import Header from '@/components/Header/Header'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  document.title = "OLX - Buy and Sell for free anywhere in Pakistan with OLX"
+  // document.title = "OLX - Buy and Sell for free anywhere in Pakistan with OLX"
   return( 
     <>
       <Component {...pageProps} />
